e3b78788ab34a8d94882d82bf845457424083647
tzzzzzn <kumartharun085@gmail.com>
2020-07-01 11:33:21 +0530

    second commit pushing into repository using pull request

---
+++ C and DataStructures/2_formatspecifier.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num=143;
+    printf("%d\n",num);
+    printf("%6d\n",num);
+    float n=143.143;
+    printf("%f\n",n);
+    printf("%6f\n",n);
+    printf("%.4f\n",n);
+    printf("%6.4f\n",n);
+}
